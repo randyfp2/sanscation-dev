a410906b2682fade3c5dd540f7f254ea8c5fad4c
Randy Farhan <randyfp2@gmail.com>
2021-06-22 14:56:43 +0700

    Section Hero dan numbering

--- src/pages/landingPage.js
+++ src/pages/landingPage.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react'
 import Header from 'parts/header'
-import { render } from '@testing-library/react';
+import { render } from '@testing-library/react'
+import homePageJSON from 'json/landingPage'
+import Hero from 'parts/hero'
 
 export default class landingPage extends Component {
   render() {
     return (
     <>
        <Header {...this.props}></Header>
+       <Hero data={homePageJSON.hero} />
      </> 
      );
     }
