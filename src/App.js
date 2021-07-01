1659bcd819a8066c3122d1f6aa4934268701db43
Randy Farhan <randyfp2@gmail.com>
2021-07-01 16:04:51 +0700

    Kembalikan ke standar development

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import landingPage from "pages/landingPage";
 import HalamanDetail from "pages/halamanDetail";
 import Checkout from "pages/checkout";
